0592880664ae48167d97615a80fc2e356fadc286
Jose Diaz <jose.uk94@gmail.com>
2026-09-11 09:49:04 +0200

    feat: centralize portfolio data in service with signals and refactor about with @for loops

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { PortfolioService } from '../shared/services/portfolio.service';
 
 @Component({
     selector: 'app-about',
@@ -8,5 +9,9 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
     styleUrl: './about.component.css'
 })
 export class AboutComponent {
+    private readonly portfolioService = inject(PortfolioService);
 
+    readonly experiences = this.portfolioService.experiences;
+    readonly education = this.portfolioService.education;
+    readonly languages = this.portfolioService.languages;
 }
